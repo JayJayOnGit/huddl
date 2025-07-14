e7410118c88565b3f9156730519edce781479a9c
JayJayOnGit <jasonaguendia@gmail.com>
2025-07-15 00:06:33 +0100

    nextjs google fonts

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { JetBrains_Mono } from "next/font/google";
+
+const font = JetBrains_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={font.className + " w-full h-full"}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
